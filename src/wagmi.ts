5f161972e498eb8b9975826e28d6bfabedd16579
imollov <ivomollov@gmail.com>
2023-05-22 13:01:29 +0300

    chore: init nextjs-wagmi-rainbowkit template
    * move walletconnect project id to env var
    * replace goerli with sepolia as dev testnet

---
+++ src/wagmi.ts
@@ -0,0 +1,28 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { sepolia, mainnet } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [sepolia] : [])],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
+    publicProvider(),
+  ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'web3-starter',
+  chains,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID!,
+})
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }

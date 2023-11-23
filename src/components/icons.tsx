08138f3fdd7efaf6af6e32eebf8c1f8a4c6bb499
imollov <ivomollov@gmail.com>
2023-11-23 17:56:21 +0200

    feat: update icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -25,24 +25,13 @@ import {
   User,
   X,
   type LucideIcon,
+  ToyBrick,
 } from 'lucide-react'
 
 export type Icon = LucideIcon
 
 export const Icons = {
-  logo: (props: LucideProps) => (
-    <svg
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-    </svg>
-  ),
+  logo: ToyBrick,
   close: X,
   spinner: Loader2,
   chevronLeft: ChevronLeft,

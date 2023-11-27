f1dbf17b347fe71c450f62842a10cd264d9a63e4
imollov <ivomollov@gmail.com>
2023-11-27 17:41:37 +0200

    feat: custom wallet connect button

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,3 +1,64 @@
 'use client'
 
-export { ConnectButton } from '@rainbow-me/rainbowkit'
+import { ConnectButton as ConnectWallet } from '@rainbow-me/rainbowkit'
+import { Button } from '@/components/ui/button'
+
+export const ConnectButton = () => {
+  return (
+    <ConnectWallet.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== 'loading'
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === 'authenticated')
+        return (
+          <div
+            {...(!ready && {
+              'aria-hidden': true,
+              style: {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button variant="secondary" onClick={openConnectModal}>
+                    Connect Wallet
+                  </Button>
+                )
+              }
+              if (chain.unsupported) {
+                return (
+                  <Button variant="destructive" onClick={openChainModal}>
+                    Wrong network
+                  </Button>
+                )
+              }
+              return (
+                <Button variant="outline" onClick={openAccountModal}>
+                  {account.displayName}
+                  {account.displayBalance ? ` (${account.displayBalance})` : ''}
+                </Button>
+              )
+            })()}
+          </div>
+        )
+      }}
+    </ConnectWallet.Custom>
+  )
+}

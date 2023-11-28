b0fc10c20b1c4e0e97bb5e81a8680d11a95485d3
Adam Avramov <adam@hack.bg>
2023-11-28 10:48:59 +0200

    wip: feat: fadroma provider/context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,16 +4,18 @@ import * as React from 'react'
 import { WagmiConfig } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 
-import { chains, config } from '../wagmi'
+import Scrt from '@fadroma/scrt'
+
+const Context = React.createContext({
+  scrt: new Scrt.testnet()
+})
+
+const { Provider } = Context
 
 type ProvidersProps = {
   children: React.ReactNode
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return (
-    <WagmiConfig config={config}>
-      <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
-    </WagmiConfig>
-  )
+  return <Provider>{children}</Provider>
 }

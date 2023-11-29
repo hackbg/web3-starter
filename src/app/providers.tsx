7b9945539f13dd2d84a893f481171e6f055cbeaf
Adam Avramov <adam@hack.bg>
2023-11-29 11:29:05 +0200

    feat: fadroma provider with block height and dummy connect method

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,27 +1,79 @@
 'use client'
 import * as React from 'react'
 import * as Scrt from '@fadroma/scrt'
-import type Connection from '@fadroma/scrt'
 //import { WagmiConfig } from 'wagmi'
 //import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 //import { chains, config } from '../wagmi'
 
-const scrt = Scrt.testnet()
+export interface IContext {
+  scrt?:   Scrt.default
+  height:  number
+  update:  ()=>Promise<void>
+  connect: ()=>Promise<void>
+}
 
-console.log({scrt})
+const stub = () => { throw new Error('this function stub should be unreachable') }
 
-const { Provider, Consumer } = React.createContext({ scrt: null as null|Connection })
+export const FadromaContext = React.createContext<IContext>({
+  height:  -Infinity,
+  update:  stub,
+  connect: stub,
+})
 
-export default Consumer
+export const useFadromaContext = () => React.useContext(
+  FadromaContext
+)
 
-export function Providers(props: { children: React.ReactNode }) {
+export const {
+  Provider: FadromaProvider,
+  Consumer: FadromaConsumer,
+} = FadromaContext
+
+export function Providers (props: { children: React.ReactNode }) {
+  console.warn('render Providers')
+  const scrt = Scrt.testnet()
+  const [height, setHeight] = React.useState(-Infinity)
+  const context = {
+    scrt,
+    height,
+    update: async () => {
+      scrt.height.then(h=>setHeight(h))
+    },
+    connect: async () => {
+      console.log(Scrt)
+    }
+  }
+  context.update()
   return (
     //<WagmiConfig config={config}>
       //<RainbowKitProvider chains={chains}>
-        <Provider value={{scrt}}>{
-          props.children
-        }</Provider>
+        <FadromaContext.Provider value={context}>{props.children}</FadromaContext.Provider>
       //</RainbowKitProvider>
     //</WagmiConfig>
+  )
+}
+
+export const LocalStateContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const [predictions, setPredictionsState] = React.useState<Prediction[]>([])
+  const [error, setError] = React.useState('')
+  const [tab, setTab] = React.useState('betslip')
+  const setPredictions = (predictions: Prediction[]) => {
+    if (predictions.some((p) => liveGameStatuses.includes(p.game.status))) {
+      setError('You cannot make predictions for live Games')
+      setTimeout(() => setError(''), 5000)
+      return
+    }
+    setPredictionsState(predictions)
+  }
+  return (
+    <LocalStateContext.Provider
+      value={{ predictions, error, setPredictions, tab, setTab }}
+    >
+      {children}
+    </LocalStateContext.Provider>
   )
 }
--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,5 +1,36 @@
 'use client'
 
+import { useFadromaContext } from '@/app/providers'
+import * as Scrt from '@fadroma/scrt'
+
 //export { ConnectButton } from '@rainbow-me/rainbowkit'
 
-export const ConnectButton = () => "connect button"
+export const ConnectButton = () => {
+  const {
+    scrt: { identity } = {},
+    connect,
+  } = useFadromaContext()
+  if (identity) {
+    return <>
+      <Height />
+      connected as {identity.name}
+    </>
+  } else {
+    return <>
+      <Height />
+      <button className="bg-primary text-primary-foreground text-sm rounded-md px-2 py-1" onClick={connect}>
+        connect to<> </>
+        <strong>Secret Network</strong>
+      </button>
+    </>
+  }
+}
+
+export const Height = () => {
+  const { height } = useFadromaContext()
+  if (height < 0) {
+    return <></>
+  } else {
+    return <div className="text-sm px-2 py-1">height: <strong>{height}</strong></div>
+  }
+}
